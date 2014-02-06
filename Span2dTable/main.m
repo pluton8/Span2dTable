efe10b01e216007ebcff1a17b1c3016e01913a5e
pluton <pluton.od@gmail.com>
2014-02-06 22:34:35 +0200

    Initial Commit

---
+++ Span2dTable/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Span2dTable
+//
+//  Created by u on 2014-02-06.
+//  Copyright (c) 2014 yes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
