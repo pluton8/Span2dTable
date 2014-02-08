1c0cc778bac9e30ff03334ea0bc3b5ae2271f265
pluton <pluton.od@gmail.com>
2014-02-08 22:21:40 +0200

    Update main program

--- Span2dTable/main.m
+++ Span2dTable/main.m
@@ -7,16 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TableSpannerAlg.h"
+
+NSString *stringForTable(NSArray *table) {
+    NSMutableString *res = [NSMutableString string];
+    for (NSArray *row in table) {
+        for (id col in row) {
+            id symbol = (col == [NSNull null]) ? @"-" : col;
+            [res appendFormat:@"%@ ", symbol];
+        }
+        [res appendString:@"\n"];
+    }
+    return [res copy];
+}
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSArray *data = @[ @[@1, @2        ],
+                           @[    @3, @4, @5] ];
+        NSDictionary *spans = @{ @1: @[@2, @1],
+                                 @2: @[@1, @3] };
+
+        NSArray *actual = [[TableSpannerAlg new]
+                           tableForData:data andSpanInfo:spans];
+        NSLog(@"Result:\n%@", stringForTable(actual));
     }
     return 0;
 }
-
